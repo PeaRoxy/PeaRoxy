1f3b80881994336c241d8029d9a9b3f518fe593e
PeaRoxy <dev@pearoxy.com>
2013-06-14 23:15:50 +0430

    Add Log Bug fixed

--- PeaRoxy.Windows.WPFClient/SettingTabs/Base.cs
+++ PeaRoxy.Windows.WPFClient/SettingTabs/Base.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -14,7 +15,7 @@ using System.Windows.Shapes;
 
 namespace PeaRoxy.Windows.WPFClient.SettingTabs
 {
-    public class Base : UserControl
+    public class Base : UserControl, ISynchronizeInvoke
     {
         public bool AutoSave { get; set; }
         internal bool isLoading = true;
@@ -25,5 +26,22 @@ namespace PeaRoxy.Windows.WPFClient.SettingTabs
             this.IsEnabled = enable;
         }
         public Base() { }
+
+        public IAsyncResult BeginInvoke(Delegate method, object[] args)
+        {
+            throw new NotImplementedException();
+        }
+        public object EndInvoke(IAsyncResult result)
+        {
+            throw new NotImplementedException();
+        }
+        public object Invoke(Delegate method, object[] args)
+        {
+            return this.Dispatcher.Invoke(method, args);
+        }
+        public bool InvokeRequired
+        {
+            get { return true; }
+        }
     }
 }
