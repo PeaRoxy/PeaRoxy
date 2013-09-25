1e48a81785fe149eab5e32b7feed759d71360ece
PeaRoxy <dev@pearoxy.com>
2013-09-26 02:05:06 +0330

    Icon added for PeaRoxy Server

--- PeaRoxy.Server/Properties/AssemblyInfo.cs
+++ PeaRoxy.Server/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PeaRoxy Server")]
+[assembly: AssemblyTitle("PeaRoxy Server (Multi-Platform)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PeaRoxy")]
 [assembly: AssemblyProduct("PeaRoxy")]
-[assembly: AssemblyCopyright("Copyright © PeaRoxy 2012")]
+[assembly: AssemblyCopyright("Copyright © PeaRoxy 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
