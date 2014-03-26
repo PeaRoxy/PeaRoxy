1f132f4ae9cba4784823d23510f31183e09c552e
PeaRoxy <dev@pearoxy.com>
2014-03-26 19:55:15 +0430

    Fixed: a bug in About page resulting in program crash

--- PeaRoxy.Windows.WPFClient/SettingTabs/About.xaml.cs
+++ PeaRoxy.Windows.WPFClient/SettingTabs/About.xaml.cs
@@ -49,49 +49,6 @@ namespace PeaRoxy.Windows.WPFClient.SettingTabs
         public About()
         {
             this.InitializeComponent();
-            try
-            {
-                this.MainVersion.Content = this.MainVersion.Content.ToString()
-                    .Replace("%version", Assembly.GetExecutingAssembly().GetName().Version.ToString());
-                this.Libraries.Content = 
-                        "ClientLibrary.dll \r\n" + 
-                        "CoreProtocol.dll \r\n" + 
-                        "Windows.dll \r\n" +
-                        "Windows.Network.TAP.dll \r\n" + 
-                        // "Windows.Network.Hook.exe \r\n" +
-                        "CommonLibrary.dll \r\n" + 
-                        "Thriple.dll (3D Controls) \r\n" +
-                        "VDialog.dll (Win7 Dialogs) \r\n" + 
-                        "EasyHook.dll \r\n" +
-                        "Tun2Socks \r\n" + 
-                        "TAP Adapter \r\n" + 
-                        "Images";
-                this.Versions.Content =
-                    string.Format(
-                        "v{0}\r\n" + 
-                        "v{1}\r\n" + 
-                        "v{2}\r\n" + 
-                        "v{3}\r\n" + 
-                        "v{4}\r\n" + 
-                        "v{5} © Josh Smith\r\n" +
-                        "v{6} © Łukasz Świątkowski\r\n" + 
-                        "v{7} © EasyHook Team\r\n" + 
-                        "© Ambroz Bizjak\r\n" +
-                        "© OpenVPN.net\r\n" + 
-                        "© Icons8.com",
-                        Assembly.GetAssembly(typeof(ProxyController)).GetName().Version.ToString(3),
-                        Assembly.GetAssembly(typeof(PeaRoxyProtocol)).GetName().Version.ToString(3),
-                        Assembly.GetAssembly(typeof(WindowsModule)).GetName().Version.ToString(3),
-                        Assembly.GetAssembly(typeof(TapTunnelModule)).GetName().Version.ToString(3),
-                        Assembly.GetAssembly(typeof(Common)).GetName().Version.ToString(3),
-                        Assembly.GetAssembly(typeof(Panel3D)).GetName().Version.ToString(3),
-                        Assembly.GetAssembly(typeof(VDialog)).GetName().Version.ToString(3),
-                        Assembly.GetAssembly(typeof(RemoteHooking)).GetName().Version.ToString(3));
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
         }
 
         #endregion
@@ -158,6 +115,36 @@ namespace PeaRoxy.Windows.WPFClient.SettingTabs
                 MessageBoxButtons.OK,
                 MessageBoxIcon.Information);
             App.End();
+        }
+
+        private void BaseLoaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.MainVersion.Content = this.MainVersion.Content.ToString()
+                    .Replace("%version", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+                this.Libraries.Content =
+                        "Thriple.dll (3D Controls) \r\n" +
+                        "VDialog.dll (Win7 Dialogs) \r\n" +
+                        "EasyHook.dll \r\n" +
+                        "Tun2Socks \r\n" +
+                        "Dns2Socks \r\n" +
+                        "TAP Adapter \r\n" +
+                        "Images";
+                this.Versions.Content =
+                    string.Format(
+                        "v{0} © Josh Smith\r\n" +
+                        "v{1} © Łukasz Świątkowski\r\n" +
+                        "v{2} © EasyHook Team\r\n" +
+                        "© Ambroz Bizjak\r\n" +
+                        "SourceForge.net/projects/dns2socks\r\n" +
+                        "© OpenVPN.net\r\n" +
+                        "© Icons8.com",
+                        Assembly.GetAssembly(typeof(Panel3D)).GetName().Version.ToString(3),
+                        Assembly.GetAssembly(typeof(VDialog)).GetName().Version.ToString(3),
+                        Assembly.GetAssembly(typeof(RemoteHooking)).GetName().Version.ToString(3));
+            }
+            catch { }
         }
     }
 }
